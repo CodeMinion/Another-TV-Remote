2c7c8f23a683327db4be76c55d55c4017e184a3f
Nicolae Iosif <iosif@bringes.io>
2022-12-30 23:45:25 +0200

    feat: decode all keyCode types

--- android/src/main/kotlin/com/rouninlabs/another_tv_remote/TvRemoteEventProcessor.kt
+++ android/src/main/kotlin/com/rouninlabs/another_tv_remote/TvRemoteEventProcessor.kt
@@ -2,41 +2,24 @@ package com.rouninlabs.another_tv_remote
 
 import android.view.KeyEvent
 import io.flutter.plugin.common.EventChannel
+import java.lang.reflect.Field
+import java.lang.reflect.Modifier
 
 object TvRemoteEventProcessor: EventChannel.StreamHandler {
     private var mEventSink:EventChannel.EventSink? = null;
+    private var keyCodeNameByKeyCode = KeyEvent::class.java.fields
+        .filter { field -> field.name.startsWith("KEYCODE_") }
+        .filter { field -> Modifier.isStatic(field.modifiers) }
+        .map { field -> field.getInt(null) to field.name }
+        .toMap();
 
     private fun setEventNotifier(sink:EventChannel.EventSink?) {
         mEventSink = sink
     }
 
     fun notifyEvent(event:KeyEvent) {
-        // TODO Call convert to generic event.
         // TODO call sink
-        val buttonKey:String =
-            when(event.keyCode) {
-                KeyEvent.KEYCODE_BUTTON_B,
-                KeyEvent.KEYCODE_BACK -> "back"
-
-                KeyEvent.KEYCODE_BUTTON_SELECT,
-                KeyEvent.KEYCODE_BUTTON_A,
-                KeyEvent.KEYCODE_ENTER,
-                KeyEvent.KEYCODE_DPAD_CENTER,
-                KeyEvent.KEYCODE_NUMPAD_ENTER -> "ok"
-
-                KeyEvent.KEYCODE_DPAD_UP -> "dPadUp"
-                KeyEvent.KEYCODE_DPAD_DOWN -> "dPadDown"
-                KeyEvent.KEYCODE_DPAD_LEFT -> "dPadLeft"
-                KeyEvent.KEYCODE_DPAD_RIGHT -> "dPadRight"
-
-                KeyEvent.KEYCODE_BUTTON_R1 -> "skipForward"
-                KeyEvent.KEYCODE_BUTTON_L1 -> "skipBackward"
-
-                KeyEvent.KEYCODE_BUTTON_R2 -> "fastForward"
-                KeyEvent.KEYCODE_BUTTON_L2 -> "rewind"
-
-                else -> "unknown"
-        }
+        val buttonKey:String = keyCodeNameByKeyCode[event.keyCode] ?: "unknown"
 
         val actionKey:String = when(event.action) {
             KeyEvent.ACTION_UP -> "up"
