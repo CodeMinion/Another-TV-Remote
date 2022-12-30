b81009a42dbbc15c515ec4b7907e92f671c848fe
Nicolae Iosif <iosif@bringes.io>
2022-12-30 23:43:00 +0200

    fix: fix the example project
    Fixes #1

--- example/android/app/src/main/kotlin/com/rouninlabs/another_tv_remote_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/rouninlabs/another_tv_remote_example/MainActivity.kt
@@ -1,6 +1,37 @@
 package com.rouninlabs.another_tv_remote_example
 
 import io.flutter.embedding.android.FlutterActivity
+import android.view.KeyEvent
+import com.rouninlabs.another_tv_remote.TvRemoteEventProcessor
 
 class MainActivity: FlutterActivity() {
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        TvRemoteEventProcessor.notifyEvent(event = event)
+        return super.onKeyDown(keyCode, event)
+    }
+    override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+        TvRemoteEventProcessor.notifyEvent(event = event)
+        return super.onKeyUp(keyCode, event)
+    }
+
+    override fun dispatchKeyEvent(event: KeyEvent): Boolean {
+
+        when(event.keyCode) {
+            KeyEvent.KEYCODE_BUTTON_B,
+            KeyEvent.KEYCODE_BACK,
+
+            KeyEvent.KEYCODE_BUTTON_SELECT,
+            KeyEvent.KEYCODE_BUTTON_A,
+            KeyEvent.KEYCODE_ENTER,
+            KeyEvent.KEYCODE_DPAD_CENTER,
+            KeyEvent.KEYCODE_NUMPAD_ENTER -> {
+                // Skip the center pad because you get it on the on onKeyDown
+                return super.dispatchKeyEvent(event)
+            }
+        }
+        if(event.action == KeyEvent.ACTION_DOWN) {
+            TvRemoteEventProcessor.notifyEvent(event = event)
+        }
+        return super.dispatchKeyEvent(event)
+    }
 }
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -16,45 +18,29 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _anotherTvRemotePlugin = AnotherTvRemote();
+  String _latestTvRemoteEvent = '';
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    AnotherTvRemote.getTvRemoteEvents().listen((event) => {
+        log('AnotherTvRemote event: $event'),
+        setState(() {
+          _latestTvRemoteEvent = event.toString();
+        })
+      });
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    /*
-    try {
-      platformVersion =
-          await _anotherTvRemotePlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }*/
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Android TV Remote'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Text('Press any key.. \n\n$_latestTvRemoteEvent\n'),
         ),
       ),
     );
